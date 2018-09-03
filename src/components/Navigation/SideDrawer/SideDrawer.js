47d5cf9269f00cb63063104116367dcb99266bb3
BenGedi <bengedi@gmail.com>
2018-09-04 01:39:05 +0300

    add supoert for open close sideDrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,23 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
+    const sideDrawerToggleClass = props.open ? classes.Open : classes.Close;
+    const attachedClasses = [classes.SideDrawer, sideDrawerToggleClass].join(' ');
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <React.Fragment>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </React.Fragment>
     );
 };
 
