c6867b1b791d3e6607c4f736831baee02dfe306a
BenGedi <bengedi@gmail.com>
2018-09-03 23:28:24 +0300

    add cancel and countinue buttons to the modal

--- src/components/Burger/OrderySummary/OrderySummary.js
+++ src/components/Burger/OrderySummary/OrderySummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients);
@@ -16,8 +17,8 @@ const orderSummary = (props) => {
         }
       </ul>
       <p>Continue to Checkout?</p>
-      <button>CANCEL</button>
-      <button>CONTINUE</button>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </React.Fragment>
   );
 };
