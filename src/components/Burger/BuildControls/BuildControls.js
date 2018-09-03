15d5c16d737283bf856d295cac8da15f5b308d2e
BenGedi <bengedi@gmail.com>
2018-09-03 08:27:06 +0300

    add total price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,14 +11,15 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
-  {controls.map(ctrl => (
-    <BuildControl 
-      key={ctrl.label} 
-      label={ctrl.label}
-      added={() => props.ingredientsAdded(ctrl.type)} 
-      removed={() => props.ingredientsRemoved(ctrl.type)}
-      disabled={props.disabled[ctrl.type]}/>
-  ))}
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+    {controls.map(ctrl => (
+      <BuildControl 
+        key={ctrl.label} 
+        label={ctrl.label}
+        added={() => props.ingredientsAdded(ctrl.type)} 
+        removed={() => props.ingredientsRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}/>
+    ))}
   </div>
 );
 
--- src/containers/BurgerBuilder/BugerBuilder.js
+++ src/containers/BurgerBuilder/BugerBuilder.js
@@ -66,7 +66,8 @@ class BurgerBuilder extends Component {
         <BuildControls
           ingredientsAdded={this.addIngredientHandler}
           ingredientsRemoved={this.removeIngredientHandler}
-          disabled={disabledInfo} />
+          disabled={disabledInfo}
+          price={this.state.totalPrice} />
       </React.Fragment>
     );
   }
