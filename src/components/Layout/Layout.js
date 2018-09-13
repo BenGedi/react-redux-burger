29eacff154f0a21756e964787c0d94b6ac767e04
BenGedi <bengedi@gmail.com>
2018-09-13 21:44:14 +0300

    add menu toggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,10 +13,17 @@ class Layout extends Component {
         this.setState({showSideDrawer: false})
     }
 
+    sideDrawerToggleHandler = () => {
+        // for avoid async state changes
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
+    }
+
     render () {
         return (
             <React.Fragment>
-                <Toolbar/>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
                 <SideDrawer 
                     open={this.state.showSideDrawer} 
                     closed={this.sideDrawerClosedHandler}/>
