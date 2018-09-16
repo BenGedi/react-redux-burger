812a6d47a9d31b45cc8b8ad667871c53d6630e81
Gedi, Ben <ben.gedi@verint.com>
2018-09-16 09:19:03 +0300

    Fix shouldComponentUpdate issue

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,7 +5,7 @@ import Backdrop from '../Backdrop/Backdrop'
 class Modal extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
-    return (nextProps.show !== nextState.show);
+    return (nextProps.show !== this.props.show);
   }
 
   render () {
