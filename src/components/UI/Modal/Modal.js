29795fc5b3bb3dfb049bb770bf38d60eddd3b849
Ben <bengedi@gmail.com>
2018-09-25 17:28:08 +0300

    adding spinner on sending request

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,14 +5,14 @@ import Backdrop from '../Backdrop/Backdrop'
 class Modal extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
-    return (nextProps.show !== this.props.show);
+    return (nextProps.show !== this.props.show || nextProps.children !== this.props.children);
   }
 
   render () {
     return (
       <React.Fragment>
         <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
-        <div 
+        <div
           className={classes.Modal}
           style={{
             transform: `translateY(${this.props.show ? '0': '-117vh'})`,
