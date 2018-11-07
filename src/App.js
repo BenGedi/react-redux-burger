1d614fd0312947ace90e6a277c8e84465584cb9a
Gedi, Ben <ben.gedi@verint.com>
2018-11-07 16:37:48 +0200

    adding routes for main page and checkout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BugerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-         <BurgerBuilder/>
-        </Layout>
-      </div>
-    );
-  }
+    render() {
+
+        /**
+         * The Switch in this case is not necessary, cus' it will render only one route 
+         * the exact makes the route path to match only the Root path
+         * the order of the routes in this case is not important because we are using the "exact" property and the Switch component (which is persent only one route at at time)
+         *  */
+        return (
+            <div>
+                <Layout>
+                    <Switch>
+                        <Route path="/checkout" component={Checkout}/>
+                        <Route path="/" exact component={BurgerBuilder}/>
+                    </Switch>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
