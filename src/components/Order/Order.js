772caf44586c9ec8f19e273ef0c83ef6961b9fa0
Gedi, Ben <ben.gedi@verint.com>
2018-11-08 20:05:22 +0200

    outputing the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = props => (
-    <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
-      <p>Price: USD 5.45</p>
-    </div>
-);
+const order = props => {
+    const ingredients = [];
+    const price = Number(props.price).toFixed(2);
+
+    for (let key in props.ingredients) {
+        ingredients.push({
+            name: key,
+            amount: props.ingredients[key]
+        });
+    }
+
+    const ingredientsOutput = ingredients.map(ig => {
+        return <span key={props.name} style={{
+            textTransform: 'capitalize',
+            display: 'inline-block',
+            margin: '0 8px',
+            border: '1px solid black',
+            padding: '5px'
+        }}>{ig.name} ({ig.amount})</span>
+    })
+    
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: USD {price}</p>
+        </div>
+    );
+}
 
 export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -16,7 +16,7 @@ class Orders extends Component {
       }
       console.log(fetchedOrders);
 
-      this.setState({loading: false});
+      this.setState({loading: false, orders: fetchedOrders});
 
     }).catch(err => {
       this.setState({loading: false});
@@ -25,8 +25,14 @@ class Orders extends Component {
   render() {
     return (
       <div> 
-        <Order/>
-        <Order/>
+        { 
+			this.state.orders.map(order => 
+				<Order 
+					key={order.id}
+					ingredients={order.ingredients}
+					price={order.price}/>
+			)
+        }
       </div>
     )
   }
